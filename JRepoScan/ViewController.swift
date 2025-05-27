392ede722793ecf56cd78e76e0ea3f30530e0e11
Michelli Cristina <contato@michellicristina.com.br>
2025-05-27 19:21:17 -0300

    feat: configura viewcode

---
+++ JRepoScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRepoScan
+//
+//  Created by Michelli Cristina de Paulo Lima on 27/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ JRepoScanUITests/JRepoScanUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JRepoScanUITestsLaunchTests.swift
+//  JRepoScanUITests
+//
+//  Created by Michelli Cristina de Paulo Lima on 27/05/25.
+//
+
+import XCTest
+
+final class JRepoScanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
