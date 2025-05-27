30a47266787918b2f7f51a7172ee10bdf5f2a589
Michelli Cristina <contato@michellicristina.com.br>
2025-05-27 20:20:05 -0300

    feat: cria celula da home

--- JRepoScan/Features/Home/Cell/RepoViewTableViewCell.swift
+++ JRepoScan/Features/Home/Cell/RepoViewTableViewCell.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class RepoViewTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
 }
